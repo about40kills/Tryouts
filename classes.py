19211b9533c85ba24c32ce7f49cb07ee5dc65ffe
Davis Opoku <opokudavis141@gmail.com>
2023-12-03 21:59:00 +0000

    davis

--- classes.py
+++ classes.py
@@ -1,39 +1,39 @@
-## A class is a blue-print for creating objects.
-##class BakingPan:
-#   def __init__(self, flour, sugar, special_ingredient):
-##     self.flour = flour
-##    self.sugar = sugar
-##   self.special_ingredient = special_ingredient
+# A class is a blue-print for creating objects.
+class BakingPan:
+   def __init__(self, flour, sugar, special_ingredient):
+     self.flour = flour
+     self.sugar = sugar
+     self.special_ingredient = special_ingredient
 
-#bread1 = BakingPan('soft', 20, 'wheat')
-#bread2 = BakingPan('hard', 15, 'banana')
-#print(bread2.special_ingredient)
+bread1 = BakingPan('soft', 20, 'wheat')
+bread2 = BakingPan('hard', 15, 'banana')
+print(bread2.special_ingredient)
 
 # accessing a global variable
-#school = 'KNUST'
-#class BakingPan:
-#    department = 'Computer Science Department'
-#    def __init__(self, flour, sugar, special_ingredient):
-#        self.flour = flour
-#        self.sugar = sugar
-#        self.special_ingredient = special_ingredient
+school = 'KNUST'
+class BakingPan:
+    department = 'Computer Science Department'
+    def __init__(self, flour, sugar, special_ingredient):
+        self.flour = flour
+        self.sugar = sugar
+        self.special_ingredient = special_ingredient
 
-#bread1 = BakingPan('soft', 20, 'wheat')
-#print(BakingPan.department)
+bread1 = BakingPan('soft', 20, 'wheat')
+print(BakingPan.department)
 
-#class BakingPan:
-#    unit_price = 5
-#    def __init__(self, flour, sugar, special_ingredient):
-#        self.flour = flour
-#        self.sugar = sugar
-#        self.special_ingredient = special_ingredient
+class BakingPan:
+    unit_price = 5
+    def __init__(self, flour, sugar, special_ingredient):
+        self.flour = flour
+        self.sugar = sugar
+        self.special_ingredient = special_ingredient
 
-#    def bread_name(self):
-#       return f'{self.special_ingredient}bread'
-#bread1 = BakingPan('soft', 20, 'wheat')
-#bread2 = BakingPan('hard', 15, 'coconut')
-#print(bread1.bread_name())
-#print(bread2.bread_name())
+    def bread_name(self):
+       return f'{self.special_ingredient}bread'
+bread1 = BakingPan('soft', 20, 'wheat')
+bread2 = BakingPan('hard', 15, 'coconut')
+print(bread1.bread_name())
+print(bread2.bread_name())
 class BakingPan:
     unit_price = 5
     def __init__(self, flour, sugar, special_ingredient):
