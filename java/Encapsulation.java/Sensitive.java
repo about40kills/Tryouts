146db87729badd181fc90bca4d871a56c335ea8a
about40kills <opokudavis141@gmail.com>
2024-04-18 18:22:43 +0000

    meh

---
+++ java/Encapsulation.java/Sensitive.java
@@ -0,0 +1,7 @@
+public class Sensitive {
+    public static void main(String[] args) {
+        Person myName = new Person();
+        myName.setName("Davis");
+        System.out.println("My name is " + myName.getName());
+    }
+}
